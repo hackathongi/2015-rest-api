5e3a518a6c4e63399dcadc3caf9199bf8975dc8b
Carles Jove i Buxeda <hola@carlus.cat>
2015-03-28 12:21:15 +0100

    reindex

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
-	require 'vendor/autoload.php';
-	require 'bddConnector.php';
-	require 'hackjobsUtils.php';
+  require 'vendor/autoload.php';
+  require 'bddConnector.php';
+  require 'hackjobsUtils.php';
 
-	$app = new \Slim\Slim();
+  $app = new \Slim\Slim();
 
-	$app->get('/', function ( ) use ( $app ) {
-	    echo "Hello HackaJobs";
-	});
+  $app->get('/', function ( ) use ( $app ) {
+      echo "Hello HackaJobs";
+  });
 
-	/**
-	 * Job
-	 */
-	$app->get('/job/:token', function ( $token ) use ( $app ) {
-		if ( $token ) {
-			$job = Job::find(1);
-			echo $job->to_json();
-		}
-		else {
-			$app->response->setStatus( 400 );
-		}
-	});
+  /**
+   * Job
+   */
+  $app->get('/job/:token', function ( $token ) use ( $app ) {
+    if ( $token ) {
+      $job = Job::find(1);
+      echo $job->to_json();
+    }
+    else {
+      $app->response->setStatus( 400 );
+    }
+  });
 
 
-	$app->run();
+  $app->run();
 
