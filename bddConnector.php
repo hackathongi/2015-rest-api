a223cea5d7e4aaad2e4bfffac55bd318df03b423
Alex Toro Luna <alex@volcanicinternet.com>
2015-03-28 15:36:41 +0100

    no localhost

--- bddConnector.php
+++ bddConnector.php
@@ -1,31 +1,14 @@
 <?php
 require_once 'vendor/php-activerecord/php-activerecord/ActiveRecord.php';
 
-	if ( is_localhost() ) {
-		 ActiveRecord\Config::initialize(function($cfg)
-		 {
-		     $cfg->set_model_directory('models');
-		     $cfg->set_connections(array(
-		        'development' => 'mysql://u_hackajobs:root@localhost/Hackajobs'));
-		 });
-	}
-	else {
 		ActiveRecord\Config::initialize(function($cfg)
 		 {
 		     $cfg->set_model_directory('models');
 		     $cfg->set_connections(array(
 		        'development' => 'mysql://hackajob:uiyr683d@mysql/Hackajobs'));
 		 });
-	}
 
 
-function is_localhost() {
-   $serverList = array('localhost', '127.0.0.1');
-
-	if(!in_array($_SERVER['HTTP_HOST'], $serverList)) {
-	        return true;
-	}
-}
 
 
 ?>
